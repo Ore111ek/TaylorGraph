12ecaf54ea4f9231be548ffa047b3dc61632c413
Ore111ek <61360969+Ore111ek@users.noreply.github.com>
2020-02-23 17:15:25 +0300

    Add files via upload

---
+++ moc_graphwindow.cpp
@@ -0,0 +1,219 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'graphwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../TaylorGraph/graphwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'graphwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_GraphWindow_t {
+    QByteArrayData data[28];
+    char stringdata0[479];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_GraphWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_GraphWindow_t qt_meta_stringdata_GraphWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 11), // "GraphWindow"
+QT_MOC_LITERAL(1, 12, 10), // "paintEvent"
+QT_MOC_LITERAL(2, 23, 0), // ""
+QT_MOC_LITERAL(3, 24, 12), // "QPaintEvent*"
+QT_MOC_LITERAL(4, 37, 1), // "e"
+QT_MOC_LITERAL(5, 39, 4), // "Tsin"
+QT_MOC_LITERAL(6, 44, 5), // "input"
+QT_MOC_LITERAL(7, 50, 4), // "Tcos"
+QT_MOC_LITERAL(8, 55, 6), // "Tarctg"
+QT_MOC_LITERAL(9, 62, 4), // "Texp"
+QT_MOC_LITERAL(10, 67, 4), // "Tpow"
+QT_MOC_LITERAL(11, 72, 1), // "n"
+QT_MOC_LITERAL(12, 74, 21), // "on_pushButton_clicked"
+QT_MOC_LITERAL(13, 96, 34), // "on_horizontalSlider_2_valueCh..."
+QT_MOC_LITERAL(14, 131, 5), // "value"
+QT_MOC_LITERAL(15, 137, 32), // "on_horizontalSlider_valueChanged"
+QT_MOC_LITERAL(16, 170, 34), // "on_horizontalSlider_3_valueCh..."
+QT_MOC_LITERAL(17, 205, 24), // "on_checkBox_stateChanged"
+QT_MOC_LITERAL(18, 230, 4), // "arg1"
+QT_MOC_LITERAL(19, 235, 26), // "on_checkBox_6_stateChanged"
+QT_MOC_LITERAL(20, 262, 26), // "on_checkBox_2_stateChanged"
+QT_MOC_LITERAL(21, 289, 26), // "on_checkBox_8_stateChanged"
+QT_MOC_LITERAL(22, 316, 26), // "on_checkBox_3_stateChanged"
+QT_MOC_LITERAL(23, 343, 26), // "on_checkBox_9_stateChanged"
+QT_MOC_LITERAL(24, 370, 26), // "on_checkBox_4_stateChanged"
+QT_MOC_LITERAL(25, 397, 26), // "on_checkBox_7_stateChanged"
+QT_MOC_LITERAL(26, 424, 26), // "on_checkBox_5_stateChanged"
+QT_MOC_LITERAL(27, 451, 27) // "on_checkBox_10_stateChanged"
+
+    },
+    "GraphWindow\0paintEvent\0\0QPaintEvent*\0"
+    "e\0Tsin\0input\0Tcos\0Tarctg\0Texp\0Tpow\0n\0"
+    "on_pushButton_clicked\0"
+    "on_horizontalSlider_2_valueChanged\0"
+    "value\0on_horizontalSlider_valueChanged\0"
+    "on_horizontalSlider_3_valueChanged\0"
+    "on_checkBox_stateChanged\0arg1\0"
+    "on_checkBox_6_stateChanged\0"
+    "on_checkBox_2_stateChanged\0"
+    "on_checkBox_8_stateChanged\0"
+    "on_checkBox_3_stateChanged\0"
+    "on_checkBox_9_stateChanged\0"
+    "on_checkBox_4_stateChanged\0"
+    "on_checkBox_7_stateChanged\0"
+    "on_checkBox_5_stateChanged\0"
+    "on_checkBox_10_stateChanged"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_GraphWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      20,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    1,  114,    2, 0x08 /* Private */,
+       5,    1,  117,    2, 0x08 /* Private */,
+       7,    1,  120,    2, 0x08 /* Private */,
+       8,    1,  123,    2, 0x08 /* Private */,
+       9,    1,  126,    2, 0x08 /* Private */,
+      10,    2,  129,    2, 0x08 /* Private */,
+      12,    0,  134,    2, 0x08 /* Private */,
+      13,    1,  135,    2, 0x08 /* Private */,
+      15,    1,  138,    2, 0x08 /* Private */,
+      16,    1,  141,    2, 0x08 /* Private */,
+      17,    1,  144,    2, 0x08 /* Private */,
+      19,    1,  147,    2, 0x08 /* Private */,
+      20,    1,  150,    2, 0x08 /* Private */,
+      21,    1,  153,    2, 0x08 /* Private */,
+      22,    1,  156,    2, 0x08 /* Private */,
+      23,    1,  159,    2, 0x08 /* Private */,
+      24,    1,  162,    2, 0x08 /* Private */,
+      25,    1,  165,    2, 0x08 /* Private */,
+      26,    1,  168,    2, 0x08 /* Private */,
+      27,    1,  171,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, 0x80000000 | 3,    4,
+    QMetaType::Double, QMetaType::Double,    6,
+    QMetaType::Double, QMetaType::Double,    6,
+    QMetaType::Double, QMetaType::Double,    6,
+    QMetaType::Double, QMetaType::Double,    6,
+    QMetaType::Double, QMetaType::Double, QMetaType::Int,    6,   11,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   14,
+    QMetaType::Void, QMetaType::Int,   14,
+    QMetaType::Void, QMetaType::Int,   14,
+    QMetaType::Void, QMetaType::Int,   18,
+    QMetaType::Void, QMetaType::Int,   18,
+    QMetaType::Void, QMetaType::Int,   18,
+    QMetaType::Void, QMetaType::Int,   18,
+    QMetaType::Void, QMetaType::Int,   18,
+    QMetaType::Void, QMetaType::Int,   18,
+    QMetaType::Void, QMetaType::Int,   18,
+    QMetaType::Void, QMetaType::Int,   18,
+    QMetaType::Void, QMetaType::Int,   18,
+    QMetaType::Void, QMetaType::Int,   18,
+
+       0        // eod
+};
+
+void GraphWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<GraphWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->paintEvent((*reinterpret_cast< QPaintEvent*(*)>(_a[1]))); break;
+        case 1: { double _r = _t->Tsin((*reinterpret_cast< double(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< double*>(_a[0]) = std::move(_r); }  break;
+        case 2: { double _r = _t->Tcos((*reinterpret_cast< double(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< double*>(_a[0]) = std::move(_r); }  break;
+        case 3: { double _r = _t->Tarctg((*reinterpret_cast< double(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< double*>(_a[0]) = std::move(_r); }  break;
+        case 4: { double _r = _t->Texp((*reinterpret_cast< double(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< double*>(_a[0]) = std::move(_r); }  break;
+        case 5: { double _r = _t->Tpow((*reinterpret_cast< double(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])));
+            if (_a[0]) *reinterpret_cast< double*>(_a[0]) = std::move(_r); }  break;
+        case 6: _t->on_pushButton_clicked(); break;
+        case 7: _t->on_horizontalSlider_2_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 8: _t->on_horizontalSlider_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 9: _t->on_horizontalSlider_3_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 10: _t->on_checkBox_stateChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 11: _t->on_checkBox_6_stateChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 12: _t->on_checkBox_2_stateChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 13: _t->on_checkBox_8_stateChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 14: _t->on_checkBox_3_stateChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 15: _t->on_checkBox_9_stateChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 16: _t->on_checkBox_4_stateChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 17: _t->on_checkBox_7_stateChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 18: _t->on_checkBox_5_stateChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 19: _t->on_checkBox_10_stateChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject GraphWindow::staticMetaObject = { {
+    &QMainWindow::staticMetaObject,
+    qt_meta_stringdata_GraphWindow.data,
+    qt_meta_data_GraphWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *GraphWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *GraphWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_GraphWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int GraphWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 20)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 20;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 20)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 20;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
